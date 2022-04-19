73f03a668ffc7d8315d9e864c2b52ada76761656
tracymuzembi <muzembi33@gmail.com>
2022-04-19 21:55:56 +0300

    added the check cpu usage module

--- bin/health_check.py
+++ bin/health_check.py
@@ -4,10 +4,24 @@
 #Does a system health check
 
 import shutil
+import psutil
 
-du = shutil.disk_usage("/")
-print (du)
+#check disk usage
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free/du.total*100
+    return free > 20
 
 #calculate free space
-du.free/du.total*100
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1)
+    return usage < 75
+
+#condition
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("ERROR!")
+else:
+    print("Everything is fine!")
+
+#cpu 
 
